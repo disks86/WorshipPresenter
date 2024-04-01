9194ae57b9a1f497f05f30309550ee506a8fa743
Christopher Schaefer <disks86@gmail.com>
2024-04-01 01:11:32 -0400

    Revert "Revert "Added assets.""
    This reverts commit 5c66e35e0866aa3ebab9c0159f1635d42f4a9a33.

--- WorshipPresenter/Properties/AssemblyInfo.cs
+++ WorshipPresenter/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -10,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("WorshipPresenter")]
-[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyCopyright("Copyright Christopher Joseph Dean Schaefer  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -24,6 +25,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("24.3.31.1")]
+[assembly: AssemblyFileVersion("24.3.31.1")]
 [assembly: ComVisible(false)]
+[assembly: NeutralResourcesLanguage("en-US")]
