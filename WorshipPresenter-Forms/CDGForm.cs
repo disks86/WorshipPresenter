ac5eeca6bb66f1693808ba2067f4da9615e9e7df
Christopher Schaefer <disks86@gmail.com>
2024-04-01 21:51:08 -0400

    Added CDG stub
    Added CDG stub.

---
+++ WorshipPresenter-Forms/CDGForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WorshipPresenter_Forms
+{
+    public partial class CDGForm : Form
+    {
+        public CDGForm()
+        {
+            InitializeComponent();
+        }
+
+        private void CDGForm_Paint(object sender, PaintEventArgs e)
+        {
+ 
+        }
+    }
+}
